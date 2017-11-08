00d78d2d13c3bfed772a093afe1457253bb876ca
Carlos Rojas <carlos.rojas.burgos@gmail.com>
2017-11-08 19:11:10 -0300

    favicon

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,27 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "PropertyApp_#{Rails.env}"
+  config.action_mailer.default_url_options = { host: 'goproperty-project.herokuapp.com'}
+
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.perform_deliveries = true
+
+  # Don't care if the mailer can't send.
+  config.action_mailer.raise_delivery_errors = true
+
+  config.action_mailer.default :charset => "utf-8"
+
+  ActionMailer::Base.smtp_settings = {
+   :address => "smtp.gmail.com",
+   :port => 587,
+   :authentication => :plain,
+   :domain => 'gmail.com',
+   :user_name => ENV['email'],
+   :password => ENV['email_password'],
+  }
+
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
